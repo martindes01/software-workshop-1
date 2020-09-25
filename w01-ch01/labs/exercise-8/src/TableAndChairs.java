bb7278d72da9245880336bfb28b834f4bb8f40f6
Martin de Spirlet <martindes01@gmail.com>
2020-09-25 18:27:47 +0100

    Add chapter 1 exercise 8

---
+++ w01-ch01/labs/exercise-8/src/TableAndChairs.java
@@ -0,0 +1,18 @@
+/**
+ * The TablesAndChairs class displays a table and two chairs.
+ *
+ * @author Martin
+ * @version 1.0.0
+ */
+public class TableAndChairs {
+
+	public static void main(String[] args) {
+		System.out.println("X                      X");
+		System.out.println("X                      X");
+		System.out.println("X      XXXXXXXXXX      X");
+		System.out.println("XXXXX  X        X  XXXXX");
+		System.out.println("X   X  X        X  X   X");
+		System.out.println("X   X  X        X  X   X");
+	}
+
+}
