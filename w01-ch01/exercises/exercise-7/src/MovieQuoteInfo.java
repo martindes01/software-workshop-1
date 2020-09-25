cb2066404d215bfe4a4a0a5eda319a96d32e1206
Martin de Spirlet <martindes01@gmail.com>
2020-09-25 18:14:08 +0100

    Add chapter 1 exercise 7

---
+++ w01-ch01/exercises/exercise-7/src/MovieQuoteInfo.java
@@ -0,0 +1,15 @@
+/**
+ * The MovieQuoteInfo class displays a movie quote, the movie it comes from, the
+ * character who said it, and the year of the movie.
+ *
+ * @author Martin
+ * @version 1.0.0
+ */
+public class MovieQuoteInfo {
+
+	public static void main(String[] args) {
+		System.out.println("\"Hello there.\"");
+		System.out.println("\t-- Obi-Wan Kenobi, Star Wars: Episode III -- Revenge of the Sith (2005)");
+	}
+
+}
