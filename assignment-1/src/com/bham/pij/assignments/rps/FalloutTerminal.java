87fdba131aa4438ff6a51486e9cbc6a06cd38a0e
Martin de Spirlet <martindes01@gmail.com>
2020-10-02 15:28:07 +0100

    Update assignment 1 FalloutTerminal.java

--- assignment-1/src/com/bham/pij/assignments/rps/FalloutTerminal.java
+++ assignment-1/src/com/bham/pij/assignments/rps/FalloutTerminal.java
@@ -6,170 +6,174 @@ import java.util.Scanner;
 
 public class FalloutTerminal {
 
-	/**
-	 * This method creates a single line of the terminal display.
-	 * @param startAddress The first address of the line.
-	 * @param wordProbability If less than or equal to 0.6 the line should contain one word.
-	 * @return The line for display.
-	 */
-	public String buildDisplayLine(int startAddress, float wordProbability) {
+    private static final String SYMBOLS = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+    private static final int TOTAL_SYMBOLS = SYMBOLS.length();
+    private static final int NUM_CHARACTERS_PER_SECTION = 30;
 
-		/*
-		 *  Add your code here. When you have selected a random word
-		 *  to add to the display, you must call addSelectedWord() to
-		 *  save that word. For example, if your variable holding the word is
-		 *  called `word' you would call as follows:
-		 * 	addSelectedWord(word);
-		 *
-		 */
+    /**
+     * This method inserts a random word into the specified StringBuilder.
+     *
+     * @param sb The StringBuilder into which to insert the word.
+     */
+    public void insertRandomWord(StringBuilder sb) {
+        if (sb == null) {
+            return;
+        }
 
-		return null;
-	}
+        String word;
 
-/*
- ***************************************************************************************
- */
+        // ensure word is not already selected
+        do {
+            word = getRandomWord();
+        } while (selectedWords.contains(word));
 
-	private static final int START_ADDRESS = 0x9380;
-	private static final int DISPLAY_HEIGHT = 20;
-	private static final int NUM_CHARACTERS_PER_ROW = 60;
-	private static final float WORD_PROB_1 = 0.6f;
-	private static final int WORD_LENGTH = 8;
-	private static final int NUM_GUESSES_ALLOWED = 4;
-	private static final int BYTE_SIZE = 8;
-	private static Random rand;
-	private static final String[] words = {
-			"flourish",
-			"appendix",
-			"separate",
-			"unlawful",
-			"platform",
-			"shoulder",
-			"marriage",
-			"attitude",
-			"reliable",
-			"contempt",
-			"prestige",
-			"evaluate",
-			"division",
-			"birthday",
-			"orthodox",
-			"appetite",
-			"perceive",
-			"pleasant",
-			"surprise",
-			"elephant",
-			"incident",
-			"medieval",
-			"absolute",
-			"dominate",
-			"designer",
-			"misplace",
-			"possible",
-			"graduate",
-			"solution",
-			"governor"
-	};
+        // ensure word is fully contained in section
+        int start = rand.nextInt(NUM_CHARACTERS_PER_SECTION - WORD_LENGTH);
 
-	private static String password;
+        sb.replace(start, start + WORD_LENGTH, word.toUpperCase());
 
-	private static ArrayList<String> selectedWords = new ArrayList<String>();
+        addSelectedWord(word);
+    }
 
-	private void run() {
+    /**
+     * This method creates a single line of the terminal display.
+     *
+     * @param startAddress    The first address of the line.
+     * @param wordProbability If less than or equal to 0.6 the line should contain
+     *                        one word.
+     * @return The line for display.
+     */
+    public String buildDisplayLine(int startAddress, float wordProbability) {
+        StringBuilder firstSection = new StringBuilder(NUM_CHARACTERS_PER_SECTION);
+        StringBuilder secondSection = new StringBuilder(NUM_CHARACTERS_PER_SECTION);
 
-		System.out.println(buildDisplay());
+        // fill section strings with random ASCII characters
+        for (int i = 0; i < NUM_CHARACTERS_PER_SECTION; i++) {
+            firstSection.append(SYMBOLS.charAt(rand.nextInt(TOTAL_SYMBOLS)));
+            secondSection.append(SYMBOLS.charAt(rand.nextInt(TOTAL_SYMBOLS)));
+        }
 
-		Scanner in = new Scanner(System.in);
+        // include word if wordProbability is less than or equal to threshold value
+        // select fairly which section includes the word
+        if (wordProbability <= WORD_PROB_1 / 2) {
+            insertRandomWord(firstSection);
+        } else if (wordProbability <= WORD_PROB_1) {
+            insertRandomWord(secondSection);
+        }
 
-		int guessCount = NUM_GUESSES_ALLOWED;
+        return "0x" + Integer.toHexString(startAddress).toUpperCase() + " " + firstSection.toString() + " 0x"
+                + Integer.toHexString(startAddress + BYTE_SIZE * NUM_CHARACTERS_PER_SECTION).toUpperCase() + " "
+                + secondSection.toString();
+    }
 
-		boolean done = false;
+    private static final int START_ADDRESS = 0x9380;
+    private static final int DISPLAY_HEIGHT = 20;
+    private static final int NUM_CHARACTERS_PER_ROW = 60;
+    private static final float WORD_PROB_1 = 0.6f;
+    private static final int WORD_LENGTH = 8;
+    private static final int NUM_GUESSES_ALLOWED = 4;
+    private static final int BYTE_SIZE = 8;
+    private static Random rand;
+    private static final String[] words = { "flourish", "appendix", "separate", "unlawful", "platform", "shoulder",
+            "marriage", "attitude", "reliable", "contempt", "prestige", "evaluate", "division", "birthday", "orthodox",
+            "appetite", "perceive", "pleasant", "surprise", "elephant", "incident", "medieval", "absolute", "dominate",
+            "designer", "misplace", "possible", "graduate", "solution", "governor" };
 
-		do {
-			System.out.println("> Password required.");
-			System.out.println("> Attempts remaining = " + guessCount);
-			System.out.println(">");
-			String guess = in.nextLine();
+    private static String password;
 
-			if (guess.equalsIgnoreCase(getPassword())) {
-				System.out.println("> Access granted.");
-				done = true;
-			}
+    private static ArrayList<String> selectedWords = new ArrayList<String>();
 
-			else {
-				--guessCount;
-				System.out.println("> Access denied.");
-				System.out.println("> Likeness = " + getCorrectCount(guess, password));
-			}
-		} while (guessCount > 0 && !done);
+    private void run() {
 
-		if (guessCount == 0) {
-			System.out.println("> Initiating lockout");
-		}
+        System.out.println(buildDisplay());
 
-		in.close();
-	}
+        Scanner in = new Scanner(System.in);
 
-	private int getCorrectCount(String guess, String password) {
+        int guessCount = NUM_GUESSES_ALLOWED;
 
-		int count = 0;
+        boolean done = false;
 
-		for (int i = 0; i < guess.length(); i++) {
-			if (guess.charAt(i) == password.charAt(i)) {
-				count++;
-			}
-		}
+        do {
+            System.out.println("> Password required.");
+            System.out.println("> Attempts remaining = " + guessCount);
+            System.out.println(">");
+            String guess = in.nextLine();
 
-		return count;
-	}
+            if (guess.equalsIgnoreCase(getPassword())) {
+                System.out.println("> Access granted.");
+                done = true;
+            } else {
+                --guessCount;
+                System.out.println("> Access denied.");
+                System.out.println("> Likeness = " + getCorrectCount(guess, password));
+            }
+        } while (guessCount > 0 && !done);
 
-	private String getRandomWord() {
-		return words[rand.nextInt(words.length)];
-	}
+        if (guessCount == 0) {
+            System.out.println("> Initiating lockout");
+        }
 
-	private String buildDisplay() {
+        in.close();
+    }
 
-		String ret = "";
+    private int getCorrectCount(String guess, String password) {
+        int count = 0;
 
-		int address = START_ADDRESS;
+        for (int i = 0; i < guess.length(); i++) {
+            if (guess.charAt(i) == password.charAt(i)) {
+                count++;
+            }
+        }
 
-		for (int i = 0; i < DISPLAY_HEIGHT; i++) {
+        return count;
+    }
 
-			float rf = rand.nextFloat();
+    private String getRandomWord() {
+        return words[rand.nextInt(words.length)];
+    }
 
-			String line = buildDisplayLine(address, rf);
+    private String buildDisplay() {
+        String ret = "";
 
-			ret += line + "\n";
+        int address = START_ADDRESS;
 
-			address += BYTE_SIZE * NUM_CHARACTERS_PER_ROW;
-		}
+        for (int i = 0; i < DISPLAY_HEIGHT; i++) {
+            float rf = rand.nextFloat();
 
-		setRandomPassword();
+            String line = buildDisplayLine(address, rf);
 
-		return ret;
-	}
+            ret += line + "\n";
 
-	public String getPassword() {
-		return password;
-	}
+            address += BYTE_SIZE * NUM_CHARACTERS_PER_ROW;
+        }
 
-	public void setPassword(String pw) {
-		password = pw;
-	}
+        setRandomPassword();
 
-	private void addSelectedWord(String word) {
-		selectedWords.add(word);
-	}
+        return ret;
+    }
 
-	public void setRandomPassword() {
-		password = selectedWords.get(rand.nextInt(selectedWords.size())).toLowerCase();
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public FalloutTerminal() {
-		rand = new Random(System.currentTimeMillis());
-	}
+    public void setPassword(String pw) {
+        password = pw;
+    }
 
-	public static void main(String[] args) {
-		new FalloutTerminal().run();
-	}
+    private void addSelectedWord(String word) {
+        selectedWords.add(word);
+    }
+
+    public void setRandomPassword() {
+        password = selectedWords.get(rand.nextInt(selectedWords.size())).toLowerCase();
+    }
+
+    public FalloutTerminal() {
+        rand = new Random(System.currentTimeMillis());
+    }
+
+    public static void main(String[] args) {
+        new FalloutTerminal().run();
+    }
+
 }
